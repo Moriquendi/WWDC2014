922a11104706415bf3a4d41ab029aa4517aee5c7
Michal Smialko <michal.smialko@gmail.com>
2014-04-06 11:38:52 +0200

    Added empty project.

---
+++ MichalSmialko/MichalSmialko/MMSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MMSAppDelegate.h
+//  MichalSmialko
+//
+//  Created by Michal Smialko on 4/6/14.
+//  Copyright (c) 2014 MMS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
